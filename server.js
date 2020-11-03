d19b9999c4be303478f0af6aed714f90148da737
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-11-03 23:08:55 +0300

    Change express app settings to use on root path

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use('/', serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
